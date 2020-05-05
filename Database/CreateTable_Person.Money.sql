d17cd0cf243f222daf521216f459b6c61e7c4b5f
Gaya3-SQL <64747625+Gaya3-SQL@users.noreply.github.com>
2020-05-04 23:19:14 -0500

    Create and Insert table Scripts

---
+++ Database/CreateTable_Person.Money.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [Person].[Money](
+[PersonID] [int] Not Null,
+[PersonName] [Varchar] (30) Not Null,
+[Gender] [Varchar] (15) Not Null,
+[AmountReceived] [int] Null,
+[AmountReceivedDate] [datetime] Null)
---
+++ Database/InsertScript_Person.Money.sql
@@ -0,0 +1,35 @@
+--Insert Script to load records into Person.Money table--
+
+Select * from Person.Money
+
+
+
+Insert into Person.Money
+(PersonID,PersonName,Gender,AmountReceived,AmountReceivedDate)
+Values
+(1,'Gayathri','Female',20,5/4/2020);
+
+
+Insert into Person.Money
+(PersonID,PersonName,Gender,AmountReceived,AmountReceivedDate)
+Values
+(2,'Mounika','Female',100,'2020-05-04');
+
+
+Insert into Person.Money
+(PersonID,PersonName,Gender,AmountReceived,AmountReceivedDate)
+Values
+(3,'Mahesh','male',10.3,'2020-05-04');
+
+Insert into Person.Money
+(PersonID,PersonName,Gender,AmountReceived,AmountReceivedDate)
+Values
+(4,'G.Mahesh','male',Null,'2020-05-04');
+
+
+Insert into Person.Money
+(PersonID,PersonName,Gender,AmountReceived,AmountReceivedDate)
+Values
+(4,NUll,'male',40,'2020-05-04');
+
+Select * from Person.Money
