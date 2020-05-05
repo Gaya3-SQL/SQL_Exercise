8127f44407c2ca60db541563ba722015aae5504b
Gaya3-SQL <64747625+Gaya3-SQL@users.noreply.github.com>
2020-05-05 06:30:40 -0500

    Create and Insert EmpolyeeData1 script
    A table is created for Employee and insert the data

---
+++ Database/CreateTable_EmployeeData1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [EmployeeData1](
+[RollNumber] [int] NOT NULL,
+[EmployeeID] [int] NOT NULL,
+[EmployeeName] [Varchar] (35) NOT NULL,
+[Gender] [Varchar] (10) NOT NULL,
+[EmployeePost] [VarChar] (30) NOT NULL,
+[ShiftTiming] [Varchar] (20) NOT NULL)
---
+++ Database/InsertScript_EmployeeData1.sql
@@ -0,0 +1,28 @@
+Select * from EmployeeData1
+
+Insert into EmployeeData1
+(RollNumber,EmployeeID,EmployeeName,Gender,EmployeePost,ShiftTiming)
+Values
+(1,1001,'Mounika','Female','Process Executive','Morning');
+
+
+Insert into EmployeeData1
+(RollNumber,EmployeeID,EmployeeName,Gender,EmployeePost,ShiftTiming)
+Values
+(2,1023,'Ramesh','male','Process Executive','Night');
+
+Insert into EmployeeData1
+(RollNumber,EmployeeID,EmployeeName,Gender,EmployeePost,ShiftTiming)
+Values
+(3,1045,'Sunny','male','Process Executive','Night');
+
+Insert into EmployeeData1
+(RollNumber,EmployeeID,EmployeeName,Gender,EmployeePost,ShiftTiming)
+Values
+(4,1085,'Mahesh','male','Manager','Night');
+
+
+Insert into EmployeeData1
+(RollNumber,EmployeeID,EmployeeName,Gender,EmployeePost,ShiftTiming)
+Values
+(5,1077,'Gayathri','Female','Senior Manager','Evening');
