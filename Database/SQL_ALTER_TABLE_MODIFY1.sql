668e84ecbe02c8c4ec09e6b3a2dd2aef5f1a66db
Gaya3-SQL <64747625+Gaya3-SQL@users.noreply.github.com>
2020-05-06 13:09:53 -0500

    ALTER_MODIFY and Add UNIQUE_DATABASE
    It is used to modify the data in the database by syntax and for adding the data using Using addunique syntax.

---
+++ Database/SQL_ALTER_TABLE_MODIFY1.sql
@@ -0,0 +1,7 @@
+--This query is used to modify the data by using ALTER COLUMN ColumnName Datatype--
+
+Select * from Persons
+
+
+ALTER TABLE Persons
+ALTER COLUMN FirstName Varchar (100);
---
+++ Database/SQL_UNIQUE_DATABASE.sql
@@ -0,0 +1,4 @@
+--This query is used to add the UNIQUE Data in the database--
+
+ALTER TABLE dbo.Persons
+Add UNIQUE (FirstName);
