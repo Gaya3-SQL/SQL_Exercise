092266fc817de05494e3c23dd7b55bd225deeef2
Gaya3-SQL <64747625+Gaya3-SQL@users.noreply.github.com>
2020-05-07 07:47:56 -0500

    ORDER BY ASC and DESC
    This syntax is used to sort from greater to lesses and lesses to greater

---
+++ SQL_Scripts/SQL_ORDER_BY_ASC.sql
@@ -0,0 +1,4 @@
+--This query is used to sort the values from asc to desc--
+
+Select * from HumanResources.Department
+ORDER BY Name ASC
---
+++ SQL_Scripts/SQL_ORDER_BY_DESC.sql
@@ -0,0 +1,9 @@
+--This query is used to sort the values from desc to asc---
+
+Select * from Purchasing.PurchaseOrderDetail
+WHERE OrderQty=150 
+
+Select * from Purchasing.PurchaseOrderDetail
+WHERE OrderQty=150 
+ORDER BY DueDate DESC 
+
